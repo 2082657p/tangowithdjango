dd0d3a112fb9c7d07f9fa7bdf75a7d23f17508f2
2082657p <2082657p@student.gla.ac.uk>
2015-01-25 17:58:27 +0000

    Chapter 04 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,4 +6,6 @@ def index(request):
     return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a> <br/>This tutorial was put together by Jack Parkinson, 2082657")
+    return HttpResponse("Rango says here is the about page. \
+                        <br/> <a href='/rango/'>Index</a> \
+                        <br/>This tutorial was put together by Jack Parkinson, 2082657")
