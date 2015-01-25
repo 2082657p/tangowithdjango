8c6f49871ec70cc40c67e0bda17fa32737bc26de
2082657p <2082657p@student.gla.ac.uk>
2015-01-25 19:49:29 +0000

    Chapter 05 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,13 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-<<<<<<< HEAD
-    return HttpResponse("Rango says here is the about page. \
-                        <br/> <a href='/rango/'>Index</a> \
-                        <br/>This tutorial was put together by Jack Parkinson, 2082657")
-=======
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a> <br/>This tutorial was put together by Jack Parkinson, 2082657")
->>>>>>> 040f172e153d028673435f0b4d31af0f69fbff97
+    context_dict = {'boldmessage': "Jack Parkinson, 2082657p"}
+    return render(request, 'rango/about.html', context_dict)
